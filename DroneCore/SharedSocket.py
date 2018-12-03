2bfe829c78ec20ce72c80ba593d0adb9c077c66c
Niels Balemans <balemans.niels@gmail.com>
2018-12-03 23:32:24 +0100

    unique id in backend, pol for the position and status (separate thread)

---
+++ DroneCore/SharedSocket.py
@@ -0,0 +1,20 @@
+
+import threading, socket
+
+
+class SharedSocket(threading.Thread):
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    queues = {}
+    running = True
+    lock = threading.Lock()
+
+    def connect(self,ip,port):
+        self.s.connect((ip, port))
+
+    def send_and_receive(self,data):
+        self.lock.acquire()
+        self.s.send(data)
+        data = self.s.recv(2048)
+        self.lock.release()
+        return data
