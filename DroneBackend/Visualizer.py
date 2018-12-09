bf31192bfed8f8c80d591f08d8fc67ee871bd8eb
dewub <wubben.jamie@outlook.com>
2018-12-09 16:48:09 +0100

    able to draw drones as points on the map, each drone has it`s own color, only current state of drones

--- DroneBackend/Visualizer.py
+++ DroneBackend/Visualizer.py
@@ -1,7 +1,10 @@
 import pygame
 import math
 import json
+import random
 import paho.mqtt.client as paho
+
+from Common.DBConnection import DBConnection
 
 mqtt_broker = "broker.mqttdashboard.com"
 mqtt_port = 1883
@@ -11,8 +14,6 @@ base_mqtt_topic = "smartcity/drones"
 class Visualizer:
 
     def __init__(self, width, height):
-        self.drones = {}
-
         self.width = width
         self.height = height
         self.screen_offset = 10
@@ -30,20 +31,27 @@ class Visualizer:
         self.mqtt.subscribe(base_mqtt_topic + "/#")
         self.mqtt.loop_start()
 
+        self.markers = self.init_markers()
+        self.drones = {}
+
         self.screen = pygame.display.set_mode((width, height))
         pygame.display.set_caption("Drone Simulator")
         pygame.display.flip()
 
     def mqtt_callback(self, mosq, obj, msg):
-        print(msg)
         data = json.loads(msg.payload.decode())
         if data["action"] == "position_update":
             self.drones[int(data["id"])] = data["position"]
 
         self.update();
 
-    def init_markers(self, markers):
-        self.markers = markers
+    def init_markers(self):
+        db = DBConnection()
+        # x,y,z,transitpoint
+        markers = []
+        for m in db.query("select * from point"):
+            markers.append(Marker(m[1], m[2], m[3],m[0]))
+
         for marker in markers:
             x = marker.x
             y = marker.y
@@ -56,7 +64,9 @@ class Visualizer:
             if y < self.min_y:
                 self.min_y = y
 
-    def get_delta(self, marker):
+        return markers
+
+    def calculate_screen_location(self, marker):
         distance_x = marker.x - self.min_x
         distance_y = marker.y - self.min_y
 
@@ -66,26 +76,25 @@ class Visualizer:
         delta_x = distance_x / total_distance_x
         delta_y = distance_y / total_distance_y
 
-        return [delta_x, delta_y]
+        pos_x = int(self.width * delta_x)
+        pos_y = int(self.height * delta_y)
+
+        if pos_x < 250:
+            pos_x += self.screen_offset
+        if pos_y < 250:
+            pos_y += self.screen_offset
+
+        if pos_x > 250:
+            pos_x -= self.screen_offset
+
+        if pos_y > 250:
+            pos_y -= self.screen_offset
+
+        return [pos_x, pos_y]
 
     def draw_markers(self, markers):
         for marker in markers:
-            [delta_x, delta_y] = self.get_delta(marker)
-
-            pos_x = int(self.width * delta_x)
-            pos_y = int(self.height * delta_y)
-
-            if pos_x < 250:
-                pos_x += self.screen_offset
-            if pos_y < 250:
-                pos_y += self.screen_offset
-
-            if pos_x > 250:
-                pos_x -= self.screen_offset
-
-            if pos_y > 250:
-                pos_y -= self.screen_offset
-
+            [pos_x, pos_y] = self.calculate_screen_location(marker)
             pygame.draw.circle(self.screen, (0, 0, 255), (pos_x, pos_y), 5)
 
     def quit_event(self):
@@ -97,16 +106,14 @@ class Visualizer:
     def draw_drones(self):
         for i in self.drones:
             location = self.drones[i]
-            x = location[0]
-            y = location[1]
-            z = location[2]
-            m = Marker(x, y, z, i)
-            [delta_x, delta_y] = self.get_delta(m)
-            pos_x = int(self.width * delta_x)
-            pos_y = int(self.height * delta_y)
-            pygame.draw.circle(self.screen, (0, 255, 0), (pos_x, pos_y), 5)
+            # litte bit misuse of Marker :P
+            drone = Marker(location[0], location[1], location[2], i)
+            [pos_x, pos_y] = self.calculate_screen_location(drone)
+            random.seed(i)
+            pygame.draw.circle(self.screen, (random.randint(0,255), random.randint(0,255), random.randint(0,255)), (pos_x, pos_y), 5)
 
     def update(self):
+        self.screen.fill((0, 0, 0))
         self.draw_markers(self.markers)
         self.draw_drones()
         pygame.display.update()
@@ -115,15 +122,7 @@ class Visualizer:
 if __name__ == "__main__":
     from Common.Marker import Marker
 
-    m0 = Marker(1, 1, 0, 0)
-    m1 = Marker(2, 1, 0, 1)
-    m2 = Marker(1, 2, 0, 2)
-    m3 = Marker(2, 2, 0, 3)
-    m4 = Marker(4, 3, 0, 4)
-    markers = [m0, m1, m2, m3, m4]
-
     screen = Visualizer(500, 500)
-    screen.init_markers(markers)
 
     running = True
     while running:
