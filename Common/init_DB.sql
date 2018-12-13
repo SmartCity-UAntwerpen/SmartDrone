4dd485c5f62e2c296d2605596e43f3f13a9a9940
dewub <wubben.jamie@outlook.com>
2018-12-13 12:18:54 +0100

    push

--- Common/init_DB.sql
+++ Common/init_DB.sql
@@ -3,7 +3,7 @@ use drones;
 create table point(id int(11) unsigned auto_increment primary key not null,x int(16) not null,y int(16) not null,z int(16) not null,transitpoint bool not null);
 insert into point(x,y,z,transitpoint) values(0,0,0,1);
 insert into point(x,y,z,transitpoint) values(1,0,0,1);
-insert into point(x,y,z,transitpoint) values(1,1,0,1);
+insert into point(x,y,z,transitpoint) values(1,1,0,0);
 insert into point(x,y,z,transitpoint) values(1,2,0,0);
 insert into point(x,y,z,transitpoint) values(2,2,0,1);
 insert into point(x,y,z,transitpoint) values(4,3,0,0);
