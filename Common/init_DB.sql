079ce61f9729be1c2565c68812b8bf63deedaa6a
dewub <wubben.jamie@outlook.com>
2018-12-13 12:59:41 +0100

    table point has a pointid now

--- Common/init_DB.sql
+++ Common/init_DB.sql
@@ -1,11 +1,11 @@
 create database drones;
 use drones;
-create table point(id int(11) unsigned auto_increment primary key not null,x int(16) not null,y int(16) not null,z int(16) not null,transitpoint bool not null);
-insert into point(x,y,z,transitpoint) values(0,0,0,1);
-insert into point(x,y,z,transitpoint) values(1,0,0,1);
-insert into point(x,y,z,transitpoint) values(1,1,0,0);
-insert into point(x,y,z,transitpoint) values(1,2,0,0);
-insert into point(x,y,z,transitpoint) values(2,2,0,1);
-insert into point(x,y,z,transitpoint) values(4,3,0,1);
-create table drone(id int(11) unsigned auto_increment primary key not null,droneid int(11) not null, x int(16) not null,y int(16) not null, z int(16) not null);
-insert into drone(droneid,x,y,z) values(0,1,1,2)
+create table point(id int(11) unsigned auto_increment primary key not null,pointID int(16) not null,x int(16) not null,y int(16) not null,z int(16) not null,transitpoint bool not null);
+insert into point(pointID, x,y,z,transitpoint) values(0,0,0,0,1);
+insert into point(pointID, x,y,z,transitpoint) values(1,1,0,0,1);
+insert into point(pointID, x,y,z,transitpoint) values(2,1,1,0,0);
+insert into point(pointID, x,y,z,transitpoint) values(3,1,2,0,0);
+insert into point(pointID, x,y,z,transitpoint) values(4,2,2,0,1);
+insert into point(pointID, x,y,z,transitpoint) values(5,4,3,0,1);
+create table drone(id int(11) unsigned auto_increment primary key not null,droneID int(11) not null, x int(16) not null,y int(16) not null, z int(16) not null);
+insert into drone(droneID,x,y,z) values(0,1,1,2)
